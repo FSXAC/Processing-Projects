c684daf5e815bbea82b3e3c9cc46dbdf9378e25d
Muchen He <mansurhe@outlook.com>
2017-08-07 22:48:41 -0700

    Terrain shadow uses world normal

--- TerrainFlyer/data/terrainVert.glsl
+++ TerrainFlyer/data/terrainVert.glsl
@@ -6,10 +6,12 @@ attribute vec3 normal;
 
 varying vec4 v_vertColor;
 varying vec3 v_vertNormal;
+varying vec3 v_worldNormal;
 
 void main() {
 	gl_Position = transform * position;
 
 	v_vertColor = color;
 	v_vertNormal = normal;
+    v_worldNormal = (transform * vec4(normal, 0.0)).xyz; //?
 }
